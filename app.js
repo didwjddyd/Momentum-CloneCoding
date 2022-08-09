bc4a9ea91a3c1d5dc543242ff98d38bad61b34db
didwjddyd <didwjddyd13@naver.com>
2022-08-09 18:11:09 +0900

    finish chapter 3

--- app.js
+++ app.js
@@ -1,48 +1,8 @@
 const h1 = document.querySelector(".hello:first-child h1");
 
-function handleH1Click()
+function handleTitleClick()
 {
-    h1.style.color = "blue";
-    console.log("h1 was clicked!");
+    h1.classList.toggle("active");
 }
 
-function handleMouseEnter()
-{
-    h1.style.color = "green";
-    h1.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave()
-{
-    h1.style.color = "black";
-    h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize()
-{
-     document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy()
-{
-    alert("copier!");
-}
-
-function handleWindowOffline()
-{
-    alert("SOS no WIFI");
-}
-
-function handleWindowOffline()
-{
-    alert("All good");
-}
-
-h1.addEventListener("click", handleH1Click);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("offline", handleWindowOffline);
+h1.addEventListener("click", handleTitleClick);
