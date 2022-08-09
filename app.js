496d0784b0b5cc15e8231145675070be6d697caf
didwjddyd <didwjddyd13@naver.com>
2022-08-09 14:20:57 +0900

    test for github token

--- app.js
+++ app.js
@@ -1,23 +1,48 @@
-const title = document.querySelector(".hello:first-child h1");
+const h1 = document.querySelector(".hello:first-child h1");
 
-function handleTitleClick()
+function handleH1Click()
 {
-    title.style.color = "blue";
-    console.log("Title was clicked!");
+    h1.style.color = "blue";
+    console.log("h1 was clicked!");
 }
 
 function handleMouseEnter()
 {
-    title.style.color = "green";
-    title.innerText = "Mouse is here!";
+    h1.style.color = "green";
+    h1.innerText = "Mouse is here!";
 }
 
 function handleMouseLeave()
 {
-    title.style.color = "black";
-    title.innerText = "Mouse is gone!";
+    h1.style.color = "black";
+    h1.innerText = "Mouse is gone!";
 }
 
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
+function handleWindowResize()
+{
+     document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy()
+{
+    alert("copier!");
+}
+
+function handleWindowOffline()
+{
+    alert("SOS no WIFI");
+}
+
+function handleWindowOffline()
+{
+    alert("All good");
+}
+
+h1.addEventListener("click", handleH1Click);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("offline", handleWindowOffline);
