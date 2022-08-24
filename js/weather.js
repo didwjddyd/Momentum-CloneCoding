44f90694a5f604489d558e07067db5395020f0a7
didwjddyd <didwjddyd13@naver.com>
2022-08-24 13:55:06 +0900

    complete all chapter

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,18 @@
+const API_KEY = "86f2c530b4d4ba49edec63d545cfbe26";
+
 function onGeoSuccess(position)
 {
-    console.log(position);
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        city.innerText = data.name;
+    });
 }
 
 function onGeoError()
